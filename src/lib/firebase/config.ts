bfde3e500b29690dab582e150a69e1bd4af6af34
Kartavya Patel <kartavyapatel2008@gmail.com>
2026-02-07 04:55:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,10 +2,10 @@
 // You can find this in your Firebase project settings.
 // It is strongly recommended to use environment variables for this in a real project.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBdBrER_yooVysal32Ix-C37zaoowfVh4g",
+  authDomain: "studio-5207264830-5a628.firebaseapp.com",
+  projectId: "studio-5207264830-5a628",
+  storageBucket: "studio-5207264830-5a628.appspot.com",
+  messagingSenderId: "478287114544",
+  appId: "1:478287114544:web:ec91f0586db560df94d8f5"
 };
