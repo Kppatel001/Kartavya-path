a2140d4b7423e95c5191f5f013a6fe6120316fb6
Kartavya Patel <kartavyapatel2008@gmail.com>
2026-03-15 05:37:20 +0000

    add all gujarat taluka name

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -36,9 +36,38 @@ export const districtsOfGujarat = [
 
 export const talukasByDistrict: Record<string, string[]> = {
   "Ahmedabad (અમદાવાદ)": ["Ahmedabad City", "Bavla", "Daskroi", "Detroj-Rampura", "Dhandhuka", "Dholera", "Dholka", "Mandal", "Sanand", "Viramgam"],
-  "Surat (સુરત)": ["Surat City", "Choryasi", "Olpad", "Kamrej", "Bardoli", "Mahuva", "Mandvi", "Songadh", "Vyara", "Uchchhal", "Nizar"],
-  "Vadodara (વડોદરા)": ["Vadodara City", "Dabhoi", "Karjan", "Padra", "Savli", "Sinor", "Vaghodia", "Desar"],
-  "Rajkot (રાજકોટ)": ["Rajkot City", "Gondal", "Jetpur", "Dhoraji", "Kotda Sangani", "Lodhika", "Paddhari", "Upleta", "Vinchhiya", "Jasdan"]
+  "Amreli (અમરેલી)": ["Amreli", "Babra", "Bagasara", "Dhari", "Jafrabad", "Khambha", "Kunkavav Vadia", "Lathi", "Lilia", "Rajula", "Savarkundla"],
+  "Anand (આણંદ)": ["Anand", "Anklav", "Borsad", "Khambhat", "Petlad", "Sojitra", "Tarapur", "Umreth"],
+  "Aravalli (અરવલ્લી)": ["Bayad", "Bhiloda", "Dhansura", "Malpur", "Meghraj", "Modasa"],
+  "Banaskantha (બનાસકાંઠા)": ["Amirgadh", "Bhabhar", "Danta", "Dantivada", "Deesa", "Deodar", "Dhanera", "Kankrej", "Lakhani", "Palanpur", "Suigam", "Tharad", "Vadgam", "Vav"],
+  "Bharuch (ભરૂચ)": ["Bharuch", "Ankleshwar", "Hansot", "Jambusar", "Jhagadia", "Netrang", "Vagra", "Valia", "Amod"],
+  "Bhavnagar (ભાવનગર)": ["Bhavnagar", "Gariadhar", "Ghogha", "Jesar", "Mahuva", "Palitana", "Sihor", "Talaja", "Umrala", "Vallabhipur"],
+  "Botad (બોટાદ)": ["Botad", "Barvala", "Gadhada", "Ranpur"],
+  "Chhota Udepur (છોટા ઉદેપુર)": ["Chhota Udepur", "Bodeli", "Jetpur Pavi", "Kavant", "Nasvadi", "Sankheda"],
+  "Dahod (દાહોદ)": ["Dahod", "Devgadh Baria", "Dhanpur", "Fatepura", "Garbada", "Limkheda", "Sanjeli", "Singvad", "Jhalod"],
+  "Dang (ડાંગ)": ["Ahwa", "Subir", "Waghai"],
+  "Devbhumi Dwarka (દેવભૂમિ દ્વારકા)": ["Bhanvad", "Kalyanpur", "Khambhalia", "Okhamandal (Dwarka)"],
+  "Gandhinagar (ગાંધીનગર)": ["Gandhinagar", "Dehgam", "Kalol", "Mansa"],
+  "Gir Somnath (ગીર સોમનાથ)": ["Veraval", "Kodinar", "Sutrapada", "Talala", "Una", "Gir-Gadhada"],
+  "Jamnagar (જામનગર)": ["Jamnagar", "Dhrol", "Jamjodhpur", "Jodiya", "Kalavad", "Lalpur"],
+  "Junagadh (જૂનાગઢ)": ["Junagadh City", "Junagadh Rural", "Bhesan", "Keshod", "Malia", "Manavadar", "Mangrol", "Mendarda", "Vanthali", "Visavadar"],
+  "Kheda (ખેડા)": ["Kheda", "Galteshwar", "Kapadvanj", "Kathlal", "Mahudha", "Matar", "Mehmedabad", "Nadiad", "Thasra", "Vaso"],
+  "Kutch (કચ્છ)": ["Bhuj", "Abdasa", "Anjar", "Bhachau", "Gandhidham", "Lakhpat", "Mandvi", "Mundra", "Nakhatrana", "Rapar"],
+  "Mahisagar (મહીસાગર)": ["Balasinor", "Kadana", "Khanpur", "Lunawada", "Santrampur", "Virpur"],
+  "Mehsana (મહેસાણા)": ["Mehsana", "Becharaji", "Kadi", "Kheralu", "Jotana", "Satlasana", "Unjha", "Vadnagar", "Vijapur", "Visnagar"],
+  "Morbi (મોરબી)": ["Morbi", "Halvad", "Maliya-Miyana", "Tankara", "Wankaner"],
+  "Narmada (નર્મદા)": ["Nandod (Rajpipla)", "Dediapada", "Garudeshwar", "Sagbara", "Tilakwada"],
+  "Navsari (નવસારી)": ["Navsari", "Bansda", "Chikhli", "Gandevi", "Jalalpore", "Khergam"],
+  "Panchmahal (પંચમહાલ)": ["Godhra", "Ghoghamba", "Halol", "Jambughoda", "Kalol", "Morwa Hadaf", "Shehera"],
+  "Patan (પાટણ)": ["Patan", "Chanasma", "Harij", "Radhanpur", "Sami", "Sankheswar", "Santalpur", "Sarasvati", "Sidhpur"],
+  "Porbandar (પોરબંદર)": ["Porbandar", "Kutiyana", "Ranavav"],
+  "Rajkot (રાજકોટ)": ["Rajkot City", "Rajkot Rural", "Gondal", "Jetpur", "Dhoraji", "Kotda Sangani", "Lodhika", "Paddhari", "Upleta", "Vinchhiya", "Jasdan", "Jamkandorna"],
+  "Sabarkantha (સાબરકાંઠા)": ["Himatnagar", "Idar", "Khedbrahma", "Poshina", "Prantij", "Talod", "Vadali", "Vijaynagar"],
+  "Surat (સુરત)": ["Surat City", "Choryasi", "Olpad", "Kamrej", "Bardoli", "Mahuva", "Mandvi", "Palsana", "Mangrol", "Umarpada"],
+  "Surendranagar (સુરેન્દ્રનગર)": ["Chotila", "Chudha", "Dasada", "Dhrangadhra", "Lakhtar", "Limbdi", "Muli", "Sayla", "Thangadh", "Wadhwan"],
+  "Tapi (તાપી)": ["Vyara", "Valod", "Nizar", "Uchchhal", "Songadh", "Dolvan", "Kukarmunda"],
+  "Vadodara (વડોદરા)": ["Vadodara City", "Vadodara Rural", "Dabhoi", "Karjan", "Padra", "Savli", "Sinor", "Vaghodia", "Desar"],
+  "Valsad (વલસાડ)": ["Valsad", "Dharampur", "Kaprada", "Pardi", "Umbergaon", "Vapi"]
 };
 
 export const schoolsByDistrict: Record<string, string[]> = {
